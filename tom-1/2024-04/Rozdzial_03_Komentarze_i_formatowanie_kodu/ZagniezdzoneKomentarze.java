f31603905ad5910002921038d45042618443d066
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2024-03-31 14:36:45 +0200

    Poprawa formatowania przykładu ZagniezdzoneKomentarze by było spójne z książką

--- tom-1/2024-04/Rozdzial_03_Komentarze_i_formatowanie_kodu/ZagniezdzoneKomentarze.java
+++ tom-1/2024-04/Rozdzial_03_Komentarze_i_formatowanie_kodu/ZagniezdzoneKomentarze.java
@@ -1,7 +1,6 @@
 public class ZagniezdzoneKomentarze {
   public static void main(String[] args) {
     // komentarz jednolinijkowy /* inny komentarz */
-
     /*
       komentarz wielolinijkowy
       // druga linia
