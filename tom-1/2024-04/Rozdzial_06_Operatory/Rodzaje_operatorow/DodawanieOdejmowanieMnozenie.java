40ed5a0c69c128bb5de96fc18fe617fed2865f0e
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2024-04-01 18:15:05 +0200

    Aktualizacja formatowania przykładów z Rozdziału 6, by było spójne z książką

--- tom-1/2024-04/Rozdzial_06_Operatory/Rodzaje_operatorow/DodawanieOdejmowanieMnozenie.java
+++ tom-1/2024-04/Rozdzial_06_Operatory/Rodzaje_operatorow/DodawanieOdejmowanieMnozenie.java
@@ -3,7 +3,6 @@ public class DodawanieOdejmowanieMnozenie {
     System.out.println(2 + 2);
     System.out.println(100 - 5);
     System.out.println(10 * 5);
-
     System.out.println(10 * 0 + 1);
     System.out.println(10 * (0 + 1));
   }
--- tom-1/2024-04/Rozdzial_06_Operatory/Rodzaje_operatorow/OperatoryBitowe.java
+++ tom-1/2024-04/Rozdzial_06_Operatory/Rodzaje_operatorow/OperatoryBitowe.java
@@ -2,7 +2,6 @@ public class OperatoryBitowe {
   public static void main(String[] args) {
     int x = 91; // w systemie binarnym: 1011011
     int y = 50; // w systemie binarny: 0110010
-
     System.out.println("x & y wynosi " + (x & y));
     System.out.println("x | y wynosi " + (x | y));
     System.out.println("x ^ y wynosi " + (x ^ y));
@@ -12,7 +11,6 @@ public class OperatoryBitowe {
     int a = 17; // w systemie binarnym: 10001
     System.out.println("a << 1 wynosi " + (a << 1));
     System.out.println("a << 2 wynosi " + (a << 2));
-
     System.out.println("a >> 1 wynosi " + (a >> 1));
     System.out.println("a >>> 1 wynosi " + (a >>> 1));
     System.out.println("-a >> 1 wynosi " + (-a >> 1));
