5b8faadf55e4ba83afaa31bfce63d24eb835507a
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2024-04-14 14:02:56 +0200

    Dodanie rozwiązań do zadań z Rozdziału 3

---
+++ tom-1/2024-04/Pytania_i_zadania_rozwiazania/Rozdzial_03/HelloWorldBezMain.java
@@ -0,0 +1,5 @@
+public class HelloWorldBezMain {
+  /*public static void main(String[] args) {
+    System.out.println("Witaj Świecie!");
+  }*/
+}
---
+++ tom-1/2024-04/Pytania_i_zadania_rozwiazania/Rozdzial_03/HelloWorldZKomentarzami.java
@@ -0,0 +1,12 @@
+// poniższy program wypisuje na ekran komunikat
+public class HelloWorldZKomentarzami {
+  /*
+    Początek metody main, która jest uruchamiana
+    przez Maszynę Wirtualną Java.
+  */
+  public static void main(String[] args) {
+    // ta instrukcja wypisuje na ekran
+    // przekazany jej komunikat
+    System.out.println("Witaj Świecie!");
+  }
+}
